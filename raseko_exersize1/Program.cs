4baaf5c079d219acb141045fce02d590ad61fa42
Veronika Savolainen <51487@edu.raseko.fi>
2024-02-06 14:29:31 +0200

    New constructors with 3 overloads

--- raseko_exersize1/Program.cs
+++ raseko_exersize1/Program.cs
@@ -9,11 +9,37 @@ namespace raseko_exersize1
     class Hooman
     {
         // Define properties of Hooman
-        string name;
-        int age;
-        string gender;
+        string name = "Ex Exaple";
+        int age = 30;
+        string gender = "Maaster";
+
+        // no arguments
+        Hooman()
+        {
+
+        }
+
+        // Constructor with one argument
+        Hooman(string name)
+        {
+            this.name = name;
+        }
+
+        // Constructor with 2 arguments
+        Hooman(string name, int age) 
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // 3 arguments
+        Hooman(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
     }
-    
     internal class Program
     {
         static void Main(string[] args)
