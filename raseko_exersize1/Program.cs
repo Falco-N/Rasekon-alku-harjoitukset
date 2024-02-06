61a90fd62bbce31d80e60f01bdd101ccf995117c
Veronika Savolainen <51487@edu.raseko.fi>
2024-02-06 13:21:48 +0200

    Started new school exersize.

---
+++ raseko_exersize1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace raseko_exersize1
+{
+    class Hooman
+    {
+        // Define properties of Hooman
+        string name;
+        int age;
+        string gender;
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
