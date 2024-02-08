fb4a040d1e8b898fda09da4a127c2084b1fd973a
Veronika Savolainen <51487@edu.raseko.fi>
2024-02-08 10:58:22 +0200

    New counter, main plus 3 sub

---
+++ ElectronicsDataBase.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicsDataBase._1
+{   
+    // Main Class and Descriptions
+    class Electronics
+    {
+        string name = "EXPhone";
+        string BuyDate = "00.00.0000";
+        double Price = 00.00;
+        int WarrantyPeriodMonths = 000;
+
+        // Need to add how the data will be input
+    }
+
+    // Sub Classes
+    class Phone
+    {
+
+    }
+
+    class Tablet
+    {
+
+    }
+
+    class Computer
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
